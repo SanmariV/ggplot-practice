66aee7c0a89622390c5d1d348720512947649d77
Sanmari Vivier <sanmari.vivier@gmail.com>
2020-02-29 13:02:35 +0200

    Added libraries and data

---
+++ scripts/mtcars_1.R
@@ -0,0 +1,16 @@
+#1. Load libraries
+library(tidyverse)
+library(lubridate)
+library(sanzo)
+library(tidyquant)
+
+#2. Load data
+mtcars
+
+#3. Examine data
+glimpse(mtcars)
+#4. Join data
+
+#5. Wrangle data
+
+#6. Data insights
